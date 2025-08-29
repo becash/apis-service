56f7ee1bc7c8272f562ecb211d354b5be50c8f04
becash <git@ci.md>
2025-08-29 18:22:34 +0300

    add GetProductAvailabilities use case

--- api/handlers.go
+++ api/handlers.go
@@ -2,17 +2,16 @@ package api
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/becash/apis/gen_go/swallow_channel_to_service"
-	"github.com/pkg/errors"
 )
 
 func (s *Server) GetAvailabilityOfProduct(
 	ctx context.Context,
 	input *swallow_channel_to_service.ProductAvailabilitiesRequest,
 ) (*swallow_channel_to_service.Availabilities, error) {
-	//res, err := s.useCases.GetProduct(ctx, input.GetId())
-	//return s.converter.FromBannerPublic(res), errors.Wrap(err, "Server GetBannerPublic")
-	//return nil, errors.Wrap(err, "Server GetBannerPublic")
-	return nil, errors.Wrap(nil, "Server GetBannerPublic")
+
+	//return s.useCases.GetProductAvailabilities(ctx, input.GetProductId())
+	return nil, fmt.Errorf("not implemented")
 }
